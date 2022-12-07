97e5a543208528a106843ecd1f3b2b29695ad9ec
isixe <isixe@outlook.com>
2022-12-07 11:05:05 +0800

    完善基本实体类

--- src/bean/User.java
+++ src/bean/User.java
@@ -1,16 +1,20 @@
 package bean;
 
 public class User {
-    // TODO: 2022/12/7 添加所有user字段
-    private String usernmae;
+    private String nmae;
     private String password;
+    private String avatars;
+    private int sex;
+    private int age;
+    private String phone;
+    private String email;
 
-    public String getUsernmae() {
-        return usernmae;
+    public String getNmae() {
+        return nmae;
     }
 
-    public void setUsernmae(String usernmae) {
-        this.usernmae = usernmae;
+    public void setNmae(String nmae) {
+        this.nmae = nmae;
     }
 
     public String getPassword() {
@@ -19,5 +23,58 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getAvatars() {
+        return avatars;
+    }
+
+    public void setAvatars(String avatars) {
+        this.avatars = avatars;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nmae='" + nmae + '\'' +
+                ", password='" + password + '\'' +
+                ", avatars='" + avatars + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
