920d8ca1134db9aacbba7fcd171ae3f765d80ca6
isixe <isixe@outlook.com>
2022-12-14 23:53:19 +0800

    完善注册功能

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -22,18 +22,28 @@ public class LoginServlet extends HttpServlet {
         String action = request.getParameter("action");
         String username = request.getParameter("name");
         String password = request.getParameter("password");
+        //登录验证
         if (action.equals("login")){
             UserService userService = new UserService();
             flag = userService.login(username,password);
+            if (flag){
+                request.getSession().setAttribute("username", username);
+                response.getWriter().write("登录成功，3秒后跳转主页！");
+                response.setHeader("refresh", "3;url=index.jsp");
+            }else {
+                PrintWriter out = response.getWriter();
+                out.print("<script>alert('帐号或密码错误，请重新登录...'); window.location='pages/login.jsp' </script>");
+            }
         }
 
-        if (flag){
-            request.getSession().setAttribute("username", username);
-            response.getWriter().write("登录成功，3秒后跳转主页！");
-            response.setHeader("refresh", "3;url=index.jsp");
-        }else {
-            PrintWriter out = response.getWriter();
-            out.print("<script>alert('帐号或密码错误，请重新登录...'); window.location='pages/login.jsp' </script>");
+        //注册验证
+        if (action.equals("register")){
+            UserService userService = new UserService();
+            flag = userService.register(username,password);
+            if (flag){
+                response.getWriter().write("注册成功，3秒后跳转登录界面！");
+                response.setHeader("refresh", "3;url=login.jsp");
+            }
         }
 
     }
